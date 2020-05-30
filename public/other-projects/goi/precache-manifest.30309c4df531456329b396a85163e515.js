cbfe3862f19fbf5e3353cb77008e39cca22de802
Dara Nguyen <duongnt1204@gmail.com>
2020-05-29 17:53:51 -0700

    Update Goi

---
+++ public/other-projects/goi/precache-manifest.30309c4df531456329b396a85163e515.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8be558547fb4024ed86b1ba967399b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a161e8cc008966103cb",
+    "url": "/static/css/2.8404715d.chunk.css"
+  },
+  {
+    "revision": "ca8411038f80920aa8a4",
+    "url": "/static/css/main.032c14b8.chunk.css"
+  },
+  {
+    "revision": "8a161e8cc008966103cb",
+    "url": "/static/js/2.150b6283.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.150b6283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8411038f80920aa8a4",
+    "url": "/static/js/main.742f802a.chunk.js"
+  },
+  {
+    "revision": "363d348083fdf4d7974e",
+    "url": "/static/js/runtime-main.5a5df8c9.js"
+  }
+]);
--- public/other-projects/goi/service-worker.js
+++ public/other-projects/goi/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba2515f08db3e481974ff51383bca787.js"
+  "/precache-manifest.30309c4df531456329b396a85163e515.js"
 );
 
 self.addEventListener('message', (event) => {
