a3bb0ac9481ba126179b1ec02bdfa80021a218f7
Dara Nguyen <duongnt1204@gmail.com>
2020-05-29 17:40:55 -0700

    Update Goi

---
+++ public/other-projects/goi/precache-manifest.ba2515f08db3e481974ff51383bca787.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0bcad73a08f4f928e1d06a6a344263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "572b2fc957bcad27c8aa",
+    "url": "/static/css/2.8404715d.chunk.css"
+  },
+  {
+    "revision": "99d390d566b1dc4479a2",
+    "url": "/static/css/main.032c14b8.chunk.css"
+  },
+  {
+    "revision": "572b2fc957bcad27c8aa",
+    "url": "/static/js/2.6645889d.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.6645889d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d390d566b1dc4479a2",
+    "url": "/static/js/main.fdf1b823.chunk.js"
+  },
+  {
+    "revision": "363d348083fdf4d7974e",
+    "url": "/static/js/runtime-main.5a5df8c9.js"
+  },
+  {
+    "revision": "cf8cb21dad2b780f33bb7c7b6f0191ed",
+    "url": "/static/media/confettiEmoji.cf8cb21d.png"
+  },
+  {
+    "revision": "dfcec9440f2c94506e12e343ea6f9a88",
+    "url": "/static/media/correctBtn.dfcec944.svg"
+  },
+  {
+    "revision": "f1294bc201963202abd1ac9c3fa52ecd",
+    "url": "/static/media/wrongBtn.f1294bc2.svg"
+  }
+]);
--- public/other-projects/goi/service-worker.js
+++ public/other-projects/goi/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c00e3daf7c2dd985e52b311965350b8d.js"
+  "/precache-manifest.ba2515f08db3e481974ff51383bca787.js"
 );
 
 self.addEventListener('message', (event) => {
